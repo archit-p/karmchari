1621f5e4eeab6dd8314e9d086669884628bd5e43
Archit Pandey <architpandeynitk@gmail.com>
2020-07-31 22:01:52 +0530

    repo: add verify code to repo
    Signed-off-by: Archit Pandey <architpandeynitk@gmail.com>

--- repo/Job.go
+++ repo/Job.go
@@ -10,3 +10,33 @@ func NewJob(JobType string) *Job {
 
 	return &j
 }
+
+// verify the state string
+func VerifyState(State string) bool {
+	// valid states
+	states := []string{ "start", "pause", "kill" }
+
+	// check whether command is present, and return an encoding
+	for _, c := range states {
+		if State == c {
+			return true
+		}
+	}
+
+	return false
+}
+
+// verify the type string
+func VerifyType(Type string) bool {
+	// valid types
+	types := []string{ "upload", "export", "teams" }
+
+	// check whether command is present, and return an encoding
+	for _, t := range types {
+		if Type == t {
+			return true
+		}
+	}
+
+	return false
+}
